bdfe19e3da2f00ea77f64f313c7206aff9e358bc
Tilde White <will.the.sixth@gmail.com>
2013-12-09 16:03:15 -0500

    test commit

---
+++ Puzzle League MAYHEM/Puzzle League MAYHEM/Program.cs
@@ -0,0 +1,25 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace Puzzle_League_MAYHEM {
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new PuzzleLeagueGame())
+                game.Run();
+        }
+    }
+#endif
+}
